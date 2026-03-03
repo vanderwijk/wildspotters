3a6728d4e207a6b6859d23de1ddbaf612b21449b
Johan van der Wijk <johan@vanderwijk.nl>
2026-03-03 14:09:55 +0100

    Initial Commit

---
+++ wildspotters/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  wildspotters
+//
+//  Created by Johan van der Wijk on 03/03/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ wildspotters/wildspottersApp.swift
@@ -0,0 +1,17 @@
+//
+//  wildspottersApp.swift
+//  wildspotters
+//
+//  Created by Johan van der Wijk on 03/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct wildspottersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
